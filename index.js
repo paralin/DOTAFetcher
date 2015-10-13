fcc2248196dda2ba53c1614b2219ca2c3a797c2f
Christian Stewart <christian@paral.in>
2015-10-12 20:06:19 -0400

    Properly setup newrelic

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 /**
  * Serves as an entry point to services, based on the ROLE var
  */
+if (process.env.NEW_RELIC_LICENSE_KEY) {
+  require('newrelic');
+}
 var config = require('./config');
 var role = config.ROLE;
 console.log(role);
